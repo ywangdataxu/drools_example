3568eafd01343c49e5415ca462fbaeb4fb96b825
Yong Wang <ywang@dataxu.com>
2015-06-24 13:41:53 -0400

    use agenda

--- src/main/java/com/drools/cashflow/CashFlowRule.java
+++ src/main/java/com/drools/cashflow/CashFlowRule.java
@@ -3,11 +3,11 @@ package com.drools.cashflow;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.StatelessKieSession;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.Agenda;
 
 public class CashFlowRule {
     public static void main(String[] args) throws ParseException {
@@ -15,7 +15,7 @@ public class CashFlowRule {
 
         KieServices kieServices = KieServices.Factory.get();
         KieContainer kContainer = kieServices.getKieClasspathContainer();
-        StatelessKieSession kSession = kContainer.newStatelessKieSession();
+        KieSession kSession = kContainer.newKieSession();
 
         Account acc = new Account();
         acc.setAccountNum(123L);
@@ -26,12 +26,27 @@ public class CashFlowRule {
         ap.setStart(format.parse("05/01/2015"));
         ap.setEnd(format.parse("08/01/2015"));
 
-        CashFlow cashFlow = new CashFlow();
-        cashFlow.setAccountNum(123L);
-        cashFlow.setAmount(2);
-        cashFlow.setType(CashFlow.Type.CREDIT);
-        cashFlow.setDate(format.parse("06/01/2015"));
+        CashFlow creditCashFlow = new CashFlow();
+        creditCashFlow.setAccountNum(123L);
+        creditCashFlow.setAmount(2);
+        creditCashFlow.setType(CashFlow.Type.CREDIT);
+        creditCashFlow.setDate(format.parse("06/01/2015"));
 
-        kSession.execute(Arrays.asList(new Object[] { acc, ap, cashFlow }));
+        CashFlow debitCashFlow = new CashFlow();
+        debitCashFlow.setAccountNum(123L);
+        debitCashFlow.setAmount(1);
+        debitCashFlow.setType(CashFlow.Type.DEBIT);
+        debitCashFlow.setDate(format.parse("06/01/2015"));
+
+        // kSession.execute(Arrays.asList(new Object[] { acc, ap, creditCashFlow, debitCashFlow }));
+        kSession.insert(acc);
+        kSession.insert(ap);
+        kSession.insert(creditCashFlow);
+        kSession.insert(debitCashFlow);
+
+        Agenda agenda = kSession.getAgenda();
+        agenda.getAgendaGroup("report").setFocus();
+        agenda.getAgendaGroup("calculation").setFocus();
+        kSession.fireAllRules();
     }
 }
